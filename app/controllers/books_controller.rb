ccd2d494f4c1ecf0aaf6797b515c57c2ea9e328f
@okadayuuki <oy19950908@gmail.com>
2020-07-24 21:21:07 +0900

    making

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,46 +1,41 @@
 class BooksController < ApplicationController
   def index
-  	@books = Book.all
-  end
-
-  def new
-  	@book = Book.new
+  	@books = Book.all.order('created_at DESC')
+    @book = Book.new
   end
 
   def create
   	@book = Book.new(book_params)
-  	book.save
-  	redirect_to create_book_path(@book.id)
-
-  	
+  	@book.save
+    redirect_to show_path(@book.id)
+    
   end
 
   def show
-  	@book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
-  	@book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
+
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to update_book_path(@book.id)
-  	
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to show_path(book.id)
+    
   end
 
   def destroy
-  	book = Book.find(params[:id])
-  	book.delete
-  	redirect_to books_path
-  	
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    
   end
 
   private
-
   def book_params
-  	params.require(:book).permit(:title,:body)
-  	
+  	params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+
+	root :to => 'homes#top'
  get '/top' => 'homes#top'
- get 'books' => 'books#index'
- get 'books' => 'books#new'
- resources :books 
+ get '/books' => 'books#index'
+ post '/books' => 'books#create'
+ get 'books/:id' => 'books#show',as: 'show'
+ get 'books/:id/edit' => 'books#edit', as: 'edit'
+ patch 'books/:id' => 'books#update', as:'update'
+ delete '/books/:id' => 'books#destroy',as: 'destroy'
+ resources :books,only:[:create] do
+ 
  	
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
